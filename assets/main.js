6ba81fa7b9439a82229bf35996458252cb5f4441
projectormato <k.keita811.anuy@gmail.com>
2018-08-06 21:00:15 +0900

    add print log.

--- assets/main.js
+++ assets/main.js
@@ -4,11 +4,13 @@ var $body = $('body');
 //開閉用ボタンをクリックでクラスの切替え
 $('#menu__btn').on('click', function (e) {
   $body.addClass('open');
+  console.log("btn click!");
   //いつかtoggleするようにする
 });
 
 // メニュー名以外の部分をクリックで閉じる
 $('#menu__nav').on('click', function () {
   $body.removeClass('open');
+  console.log("menu click!");
 });
 });
