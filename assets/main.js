70800a6f30c6323240694bfbeb483a9fe4468f06
projectormato <k.keita811.anuy@gmail.com>
2018-08-08 17:37:31 +0900

    v.0.7

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,14 @@
 $(function () {
 var $body = $('body');
-console.log($('#menu__btn'))
+
 //開閉用ボタンをクリックでクラスの切替え
 $('#menu__btn').on('click', function (e) {
   $body.addClass('open');
-  console.log("btn click!");
   //いつかtoggleするようにする
 });
 
 // メニュー名以外の部分をクリックで閉じる
 $('#menu__nav').on('click', function () {
   $body.removeClass('open');
-  console.log("menu click!");
 });
 });
